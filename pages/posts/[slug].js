1eedc5a74e2e6d26579fb142f25ac1b32ee6768d
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-28 16:29:32 +0100

    Added basic pages

---
+++ pages/posts/[slug].js
@@ -0,0 +1,7 @@
+const Post = () => {
+  return (
+    <h1>Hello from the Post page</h1>
+  )
+}
+
+export default Post
---
+++ pages/posts/index.js
@@ -0,0 +1,7 @@
+const Posts = () => {
+  return (
+    <h1>Hello from the Posts page</h1>
+  )
+}
+
+export default Posts
