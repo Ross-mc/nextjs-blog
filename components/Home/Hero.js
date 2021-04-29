784d8ad45f85327faca5f668a8fa71c1e53bfaf7
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-29 11:20:34 +0100

    added hero component details including image

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -1,7 +1,18 @@
+import Image from "next/image";
 import classes from "./hero.module.css"
 
+//using an Image component, we can use a path assuming we are in the public
+//folder as this is the purpose of the Image component
 const Hero = (props) => {
-  return <h1>Hero Component</h1>
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image src="/images/cropped.jpg" alt="me" width={500} height={500}/>
+      </div>
+      <h1>Hello I am Ross</h1>
+      <p>I am a web developer. Welcome to my blog.</p>
+    </section>
+  )
 }
 
 export default Hero
