92c0bf54141bc388c1a5bdd22dc8722e198f1b0b
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-30 10:11:59 +0100

    back end connection to mongodb

--- components/Contact/ContactForm.js
+++ components/Contact/ContactForm.js
@@ -1,23 +1,59 @@
+import { useRef } from "react";
 import classes from "./contactform.module.css";
 
 const ContactForm = () => {
+  const emailInput = useRef("");
+  const nameInput = useRef("");
+  const messageInput = useRef("");
+
+  const sendMessageHandler = (e) => {
+    e.preventDefault();
+
+    const email = emailInput.current.value;
+    const name = nameInput.current.value;
+    const message = messageInput.current.value;
+
+    if (
+      !email ||
+      !email.includes("a") ||
+      !name ||
+      !name.trim() === "" ||
+      !message ||
+      !message.trim() === ""
+    ) {
+      return;
+    }
+
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email,
+        name,
+        message
+      })
+    }).then(res => res.json())
+  }
+
   return (
     <section className={classes.contact}>
       <h1>How can I help?</h1>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
         <div className={classes.controls}>
           <div className={classes.control}>
             <label htmlFor="email">Email</label>
-            <input type="email" id="email" required />
+            <input type="email" id="email" required ref={emailInput}/>
           </div>
           <div className={classes.control}>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" required />
+            <input type="text" id="name" required ref={nameInput}/>
           </div>
         </div>
         <div className={classes.control}>
           <label htmlFor="message">Your Message</label>
-          <textarea name="message" id="message" rows="5"></textarea>
+          <textarea name="message" id="message" rows="5" ref={messageInput}/>
         </div>
         <div className={classes.actions}>
           <button>Send Message</button>
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import {MongoClient} from "mongodb"
+
+export default async (req, res) => {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
 
@@ -18,9 +20,33 @@ const handler = (req, res) => {
     const newMessage = {
       email, name, message
     }
+    console.log(process.env.MONGODB_URI)
 
-    console.log(newMessage);
+    let connection;
+    try {
+      connection = await MongoClient.connect(process.env.MONGODB_URI);
+      console.log(connection)
+    } catch (err) {
+      console.log('inside the first catch')
+      console.log(err)
+      res.status(500).json({message: "Internal Server Error"})
+      return
+    }
 
-    res.status(201).json({message: "Successfully stored message"})
+    const db = connection.db();
+    try {
+      const result = await db.collection("messages").insertOne(newMessage);
+      res.status(201).json({message: "Success!"})
+      connection.close()
+      return
+    } catch (error) {
+      onsole.log('inside the second catch')
+      res.status(500).json({message: "Internal Server Error"})
+      connection.close()
+      return
+    }
+    
+    
+
   }
 };
